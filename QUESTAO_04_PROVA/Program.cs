99c0eae9206a4ebdf4748e665304ee2ab2e90e34
Duarte-753 <julioduartebatista753@gmail.com>
2022-07-03 17:14:03 -0300

    Add files via upload

---
+++ QUESTAO_04_PROVA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUESTAO_04_PROVA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nome:julio Duarte Batista RA:6322046 
+
+            //4 – Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.
+
+            for (int i = 1; i < 51; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();  
+        }
+    }
+}
